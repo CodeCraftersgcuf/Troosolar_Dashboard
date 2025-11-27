19653d3f8852cb86ca68653330235bdf94200e4b
CodeCraftersgcuf <abubakarsajjad.202202655@gcuf.edu.pk>
2025-11-27 12:23:14 +0500

    config

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,7 +1,7 @@
 // src/config/api.config.js
-export const BASE_URL = 'https://troosolar.hmstech.org/api';
+// export const BASE_URL = 'https://troosolar.hmstech.org/api';
 
-// export const BASE_URL = 'http://127.0.0.1:8000/api';
+export const BASE_URL = 'http://127.0.0.1:8000/api';
 
 const API = {
     LOGIN: `${BASE_URL}/login`,
